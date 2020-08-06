4fe3e454ac32fdf2455006f816bfdffcc4f51a2e
Jose Luis <3006762980101@ingenieria.usac.edu.gt>
2020-08-06 12:06:43 -0600

    do while menu

--- main.go
+++ main.go
@@ -4,20 +4,27 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strings"
 
 	parser "./Analisis"
+	"github.com/fatih/color"
 )
 
 func main() {
+	color.HiCyan("Bienvenido a la consola de comandos")
 	Menu()
 }
 
 //Menu is...
 func Menu() {
-	Menu := "Bienvenido a la consola de comandos\n>> "
-	fmt.Print(Menu)
-	scanner := bufio.NewScanner(os.Stdin)
-	scanner.Scan() // use `for scanner.Scan()` to keep reading
-	Comando := scanner.Text()
-	parser.Analizar(Comando)
+	for {
+		fmt.Print(">> ")
+		scanner := bufio.NewScanner(os.Stdin)
+		scanner.Scan() // use `for scanner.Scan()` to keep reading
+		Comando := scanner.Text()
+		if strings.ToLower(Comando) == "salir" {
+			break
+		}
+		parser.Analizar(Comando)
+	}
 }
