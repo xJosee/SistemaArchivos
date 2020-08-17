0a3d426f120cb37f89fe1aeb637646f9d1e1706e
Jose Luis <3006762980101@ingenieria.usac.edu.gt>
2020-08-16 23:16:57 -0600

    Metodo insertar Lista Comando mount

---
+++ Estructuras/Lista.go
@@ -0,0 +1,32 @@
+package estructuras
+
+//Nodo is...
+type Nodo struct {
+	Direccion string
+	Nombre    string
+	Letra     byte
+	Num       int
+	Siguiente *Nodo
+}
+
+//Lista is...
+type Lista struct {
+	Contador int
+	Primero  *Nodo
+}
+
+//Insertar is...
+func (Lista *Lista) Insertar(Nodo *Nodo) {
+
+	Lista.Contador++
+	aux := Lista.Primero
+	if Lista.Primero == nil {
+		Lista.Primero = Nodo
+	} else {
+		for aux.Siguiente != nil {
+			aux = aux.Siguiente
+		}
+		aux.Siguiente = Nodo
+	}
+
+}
